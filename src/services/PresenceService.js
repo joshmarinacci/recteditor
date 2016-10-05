f31e16b2e8c30ee665c026b2d6d08eafde36e53c
Josh Marinacci <joshua@marinacci.org>
2016-10-05 12:13:06 -0700

    added stub for a presence service

---
+++ src/services/PresenceService.js
@@ -0,0 +1,22 @@
+/**
+ * Created by josh on 10/5/16.
+ */
+/*
+new PresenceService({
+    pubnub:pubnub,
+    channel:'my-doc-1'
+});
+*/
+
+
+/*
+connects to a channel, for presence events
+stores list of all users and their state
+sends change events when anything in the user's state changes
+
+demo shows cursors, list of users with their user names, random color chosen
+ */
+
+class PresenceService {
+
+}
